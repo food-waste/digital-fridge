42b6a10932a73a5cfcf7973f60c9e11f8e80c46d
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2019-02-19 21:04:56 -0600

    login fixes

--- login.js
+++ login.js
@@ -1,27 +1,38 @@
-$(document).ready(function(){
+var first_name;
 
+$(document).ready(function(){
    $('.message a').click(function(){
       $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
    });
-
-   
-});
-$(document).ready(function(){
-$("#login_username").keyup(function(){
-   if($("#login_username").val() == ""){
-      $(".nouser").show(); 
-   }else{
-      $(".nouser").hide();
-   }
- });
 });
 
 $(document).ready(function(){
-$("#login_password").keyup("input propertychange",function(){
-   if($("#login_password").val() != "123"){
-      $(".nopwd").show();
-   }else{
-      $(".nopwd").hide(); 
-   }
+  $('#login_button').click(function(){
+    if($('#login_username').val() == ""){
+      $('.nouser').show();
+    } if ($('#login_password').val() == "") {
+      $('.nopwd').show();
+    } else {
+      window.location.href = 'kitchen.html';
+      return false;
+    }
+  });
 });
+
+$(document).ready(function(){
+  $('#create_button').click(function(){
+    if($('#first_name').val() == ""){
+      $('.noname').show();
+    } if ($('#create_username').val() == "") {
+      $('.nouserc').show();
+    } if ($('#create_password').val() == "") {
+      $('.nopwdc').show();
+    } if ($('#create_email').val() == "") {
+      $('.noemail').show();
+    } else {
+      first_name = $('#first_name').val();
+      window.location.href = 'kitchen.html';
+      return false;
+    }
+  });
 });
