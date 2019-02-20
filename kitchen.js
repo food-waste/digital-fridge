954ceef5f0f7f27bf0449ae739ab0a31ed3aeac2
feng <Chunlinfeng2020@u.northwestern.edu>
2019-02-19 21:19:08 -0600

    kitchen.js

--- kitchen.js
+++ kitchen.js
@@ -1,27 +1,11 @@
 $(document).ready(function(){
 
-   $('.message a').click(function(){
-      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-   });
 
-   
-});
-$(document).ready(function(){
-$("#login_username").keyup(function(){
-   if($("#login_username").val() == ""){
-      $(".nouser").show(); 
-   }else{
-      $(".nouser").hide();
-   }
- });
 });
 
-$(document).ready(function(){
-$("#login_password").keyup("input propertychange",function(){
-   if($("#login_password").val() != "123"){
-      $(".nopwd").show();
-   }else{
-      $(".nopwd").hide(); 
-   }
-});
-});
+
+function appendTable(){
+   var txt1="<table><tr><td>Test</td><td>Test</td><td>Test</td></tr></table>";
+
+   $("body").append(txt1);
+}
