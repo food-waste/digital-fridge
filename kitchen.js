13cfe98d205c5818d86914a13caad8834bf4e516
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2019-02-19 22:18:39 -0600

    kitchen.js

--- kitchen.js
+++ kitchen.js
@@ -14,13 +14,15 @@ $("#login_password").keyup("input propertychange",function(){
 });
 
 function manualInput(){
-   var name = document.getElementById("name").value;
-   var purchaseDate = document.getElementById("purchase").value;
-   var expireDate = document.getElementById("expiry").value;
+   // var name = document.getElementById("name").value;
+   // var purchaseDate = document.getElementById("purchase").value;
+   // var expireDate = document.getElementById("expiry").value;
    
    var mytable = document.getElementById("tableId");
-   alert("name");
-   var newRow = mytable.insertRow(mytable.getElementsByTagName("tr").length);
+   
+   //mytable.getElementsByTagName("tr").length
+   var newRow = mytable.insertRow(0);
+   
    var cellA = newRow.insertCell(0);
    var cellB = newRow.insertCell(1);
    var cellC = newRow.insertCell(2);
@@ -28,7 +30,9 @@ function manualInput(){
    cellA.innerHTML = "name";
    cellB.innerHTML = "purchaseDate";
    cellC.innerHTML = "expireDate";
+   
 }
+
 function appendTable(){
    var txt1="<table><tr><td>Test</td><td>Test</td><td>Test</td></tr></table>";
 
