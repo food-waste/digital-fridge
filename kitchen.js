59f8b0875484498f8bb3c71e9685c057ede05966
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2019-02-19 21:42:56 -0600

    kitchen.js manualinput

--- kitchen.js
+++ kitchen.js
@@ -25,20 +25,20 @@ $("#login_password").keyup("input propertychange",function(){
    }
 });
 });
-$(document).ready(function(){
+
 function manualInput(){
    var name = document.getElementById("name").value;
    var purchaseDate = document.getElementById("purchase").value;
    var expireDate = document.getElementById("expiry").value;
-
+   
    var mytable = document.getElementById("tableId");
-   var newRow = mytable.insertRow(mytable.getElementsByTagName("tr").length)
+   alert("name");
+   var newRow = mytable.insertRow(mytable.getElementsByTagName("tr").length);
    var cellA = newRow.insertCell(0);
    var cellB = newRow.insertCell(1);
    var cellC = newRow.insertCell(2);
-
+   
    cellA.innerHTML = "name";
    cellB.innerHTML = "purchaseDate";
    cellC.innerHTML = "expireDate";
 }
-)}
