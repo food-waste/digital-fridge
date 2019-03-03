4bf3005a857fb203245cb1298fe05b039ca76d37
feng <Chunlinfeng2020@u.northwestern.edu>
2019-03-03 14:55:01 -0600

    Real Time Search

--- pages.js
+++ pages.js
@@ -404,5 +404,23 @@ function sortTable(n) {
 }
 
 // ********************************************
-// Real Time Search(RTS) Function
+// Real Time Search(RT_Search) Function
 // *********************************************
+function RT_Search() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("Target");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tableId");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
